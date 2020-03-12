9b9f72cc5aee2a7cb114b99ac1f06d09e65965f8
hyvu <hyvu1998@gmail.com>
2020-03-12 14:29:03 +0700

    onMeasure

--- app/src/main/java/com/example/hyvu/customview/CustomView.java
+++ app/src/main/java/com/example/hyvu/customview/CustomView.java
@@ -5,7 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.icu.util.Measure;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,12 +15,13 @@ import java.util.ArrayList;
 
 public class CustomView extends View {
 
-    final static String CONTENT="Android offers a sophisticated and powerful componentizedeeeee model for building your UI," +
+    final static String CONTENT="Android offers a sophisticated and powerful componentize model for building your UI," +
             " based on the fundamental layout classes: View and ViewGroup.";
 
 
     private Paint squarePaint,textPaint;
     private ArrayList<String> linesContent;
+    private int countLine;
 
     public CustomView(Context context) {
         super(context,null);
@@ -31,64 +34,86 @@ public class CustomView extends View {
         textPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
         squarePaint.setColor(Color.BLUE);
         textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(40);
         linesContent = new ArrayList<>();
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        linesContent.clear();
+        StringMeasure(widthMeasureSpec);
+        int width = measureWidth(widthMeasureSpec);
+        int height = measureHeight(heightMeasureSpec,widthMeasureSpec);
+        setMeasuredDimension(width,height);
+    }
 
+    private int measureHeight(int heightMeasureSpec,int widthMeasureSpec ){
+        int result = 0;
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec`.getSize(heightMeasureSpec);
+        if(heightMode == MeasureSpec.EXACTLY){
+            result = heightSize;
+        }
+        else {
+            result = Math.round(
+                    ((textPaint.descent()-textPaint.ascent())*(countLine))+
+                    ((textPaint.descent())*(countLine/2)) +
+                    MeasureSpec.getSize(widthMeasureSpec) +
+                    getPaddingRight() +
+                    getPaddingLeft());
+            if(heightMode == MeasureSpec.AT_MOST){
+                result = Math.min(result,heightSize);
+            }
+        }
+
+        return result;
+    }
+
+    private int measureWidth(int widthMeasureSpec){
+        int result = 0;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        if(widthMode == MeasureSpec.EXACTLY){
+            result = widthSize;
+        }
+        else{
+            result = (int) (textPaint.measureText(CONTENT) + getPaddingLeft() + getPaddingRight());
+            if(widthMode == MeasureSpec.AT_MOST){
+                result = Math.min(result,widthSize);
+            }
+        }
+        return result;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         int canvasWidth = getWidth();
-        int canvasHeight= getHeight();
-        float centerView= canvasWidth * 0.5f;
         canvas.drawRect(0,0,canvasWidth,getWidth(),squarePaint);
-
-        textPaint.measureText(CONTENT);
-        textPaint.setTextSize(40);
-        String t=CONTENT;
-        String line="";
-        float lengthText = textPaint.measureText(t);
-
-        Rect rect = new Rect();
-        canvas.getClipBounds(rect);
-
-        int ratio = (int) ((lengthText/rect.width())+2);
-        Toast.makeText(getContext(),textPaint.measureText(t)+"",Toast.LENGTH_SHORT).show();
-
-        while(ratio!=0){
-
-//            int newLength = textPaint.breakText(t,true,canvasWidth,null);
-//            line = t.substring(0,newLength);
-//            newLength=line.lastIndexOf(" ");
-//            newLength = newLength +1;
-//            String k = t.substring(newLength);
-//            for(int i=0;i<k.length();i++){
-//                if(k.charAt(i) == ' '){
-//                    k=t.substring(newLength,i);
-//                }
-//            }
-//            ratio--;
-
-            int newLength = textPaint.breakText(t,true,canvasWidth,null);
-            line = t.substring(0,newLength);
-            if(ratio>1){
-                newLength=line.lastIndexOf(" ");
-                linesContent.add(t.substring(0,newLength));
-                newLength = newLength +1;
-                t = t.substring(newLength);
-            }
-            else{
-                linesContent.add(t.substring(0,newLength));
-                t = t.substring(newLength);
-            }
-            ratio--;
-        }
-        float canvasText = canvasWidth+(textPaint.descent()*4);
-        for(int i=0;i<linesContent.size();i++){
+        float canvasText = canvasWidth;
+        for(int i=0;i<countLine;i++){
+            canvasText+=((textPaint.descent()*4.5f));;
             canvas.drawText(linesContent.get(i),0,canvasText,textPaint);
             textPaint.descent();
-            canvasText+=((textPaint.descent()*4.5f));;
         }
+    }
+
+    void StringMeasure(int canvasWidth){
+        canvasWidth = MeasureSpec.getSize(canvasWidth);
+        textPaint.measureText(CONTENT);
+        String t=CONTENT;
+        while(t.length()>0){
+            int breakIndex = textPaint.breakText(t,true,canvasWidth,null);
+            if(t.length() <= breakIndex){}
+            else if(t.charAt(breakIndex) == ' ' && t.length() > breakIndex){ }
+            else{
+                while(t.charAt(breakIndex-1) != ' '){
+                    breakIndex--;
+                }
+            }
+            linesContent.add(t.substring(0,breakIndex));
+            t = t.substring(breakIndex);
+        }
+        countLine = linesContent.size();
     }
 }
